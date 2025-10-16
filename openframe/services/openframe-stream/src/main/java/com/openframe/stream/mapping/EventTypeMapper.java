98b1d66a4347f02ce5a948f9d1fb104e0d68fb1b
Arsenii-Malov <arsenij@flamingo.cx>
2025-10-16 22:29:25 +0300

    update fleet events (#643)

--- openframe/services/openframe-stream/src/main/java/com/openframe/stream/mapping/EventTypeMapper.java
+++ openframe/services/openframe-stream/src/main/java/com/openframe/stream/mapping/EventTypeMapper.java
@@ -147,24 +147,177 @@ public class EventTypeMapper {
         registerMapping(IntegratedToolType.TACTICAL, SourceEventTypes.Tactical.SITE_DELETE, UnifiedEventType.GROUP_DELETED);
 
         // Fleet MDM mappings (activity_type column values)
+        
+        // Authentication Events
         registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.USER_LOGGED_IN, UnifiedEventType.LOGIN);
         registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.USER_FAILED_LOGIN, UnifiedEventType.LOGIN_FAILED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.USER_ADDED_BY_SSO, UnifiedEventType.USER_CREATED);
+        
+        // User Management Events
         registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.CREATED_USER, UnifiedEventType.USER_CREATED);
-        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.CHANGED_USER_GLOBAL_ROLE, UnifiedEventType.USER_ROLE_CHANGED);
-        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.FLEET_ENROLLED, UnifiedEventType.DEVICE_REGISTERED);
-        // Generic fallbacks / additional common Fleet events
         registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.DELETED_USER, UnifiedEventType.USER_DELETED);
-        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.EDITED_USER, UnifiedEventType.USER_UPDATED);
-        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.DELETED_HOST, UnifiedEventType.DEVICE_DELETED);
-        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.CHANGED_HOST_STATUS, UnifiedEventType.DEVICE_UPDATED);
-        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.POLICY_VIOLATION, UnifiedEventType.POLICY_VIOLATION);
-        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.APPLIED_POLICY, UnifiedEventType.POLICY_APPLIED);
-        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.POLICY_COMPLIANCE_CHECKED, UnifiedEventType.COMPLIANCE_CHECK);
-        // Remote session events if Fleet supports such activities
-        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.REMOTE_SESSION_START, UnifiedEventType.REMOTE_SESSION_START);
-        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.REMOTE_SESSION_END, UnifiedEventType.REMOTE_SESSION_END);
-        // Alerting / monitoring examples
-        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.ALERT_TRIGGERED, UnifiedEventType.ALERT_TRIGGERED);
-        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.ALERT_RESOLVED, UnifiedEventType.ALERT_RESOLVED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.CHANGED_USER_GLOBAL_ROLE, UnifiedEventType.USER_ROLE_CHANGED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.DELETED_USER_GLOBAL_ROLE, UnifiedEventType.USER_ROLE_CHANGED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.CHANGED_USER_TEAM_ROLE, UnifiedEventType.USER_ROLE_CHANGED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.DELETED_USER_TEAM_ROLE, UnifiedEventType.USER_ROLE_CHANGED);
+        
+        // Device Enrollment Events
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.FLEET_ENROLLED, UnifiedEventType.DEVICE_REGISTERED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.MDM_ENROLLED, UnifiedEventType.MDM_ENROLLED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.MDM_UNENROLLED, UnifiedEventType.MDM_UNENROLLED);
+        
+        // Activity Automations
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.ENABLED_ACTIVITY_AUTOMATIONS, UnifiedEventType.AUTOMATION_ENABLED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.EDITED_ACTIVITY_AUTOMATIONS, UnifiedEventType.AUTOMATION_UPDATED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.DISABLED_ACTIVITY_AUTOMATIONS, UnifiedEventType.AUTOMATION_DISABLED);
+        
+        // Pack Management
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.CREATED_PACK, UnifiedEventType.PACK_CREATED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.EDITED_PACK, UnifiedEventType.PACK_UPDATED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.DELETED_PACK, UnifiedEventType.PACK_DELETED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.APPLIED_SPEC_PACK, UnifiedEventType.PACK_APPLIED);
+        
+        // Policy Management
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.CREATED_POLICY, UnifiedEventType.POLICY_APPLIED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.EDITED_POLICY, UnifiedEventType.POLICY_APPLIED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.DELETED_POLICY, UnifiedEventType.POLICY_APPLIED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.APPLIED_SPEC_POLICY, UnifiedEventType.POLICY_APPLIED);
+        
+        // Saved Query Management
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.CREATED_SAVED_QUERY, UnifiedEventType.QUERY_CREATED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.EDITED_SAVED_QUERY, UnifiedEventType.QUERY_UPDATED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.DELETED_SAVED_QUERY, UnifiedEventType.QUERY_DELETED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.DELETED_MULTIPLE_SAVED_QUERY, UnifiedEventType.QUERY_DELETED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.APPLIED_SPEC_SAVED_QUERY, UnifiedEventType.QUERY_UPDATED);
+        
+        // Team Management
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.CREATED_TEAM, UnifiedEventType.GROUP_CREATED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.DELETED_TEAM, UnifiedEventType.GROUP_DELETED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.APPLIED_SPEC_TEAM, UnifiedEventType.GROUP_UPDATED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.TRANSFERRED_HOSTS, UnifiedEventType.DEVICE_UPDATED);
+        
+        // Agent Options
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.EDITED_AGENT_OPTIONS, UnifiedEventType.CONFIGURATION_UPDATED);
+        
+        // Queries
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.LIVE_QUERY, UnifiedEventType.QUERY_EXECUTED);
+        
+        // OS Version Requirements
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.EDITED_MACOS_MIN_VERSION, UnifiedEventType.CONFIGURATION_UPDATED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.EDITED_IOS_MIN_VERSION, UnifiedEventType.CONFIGURATION_UPDATED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.EDITED_IPADOS_MIN_VERSION, UnifiedEventType.CONFIGURATION_UPDATED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.EDITED_WINDOWS_UPDATES, UnifiedEventType.CONFIGURATION_UPDATED);
+        
+        // Disk Encryption
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.READ_HOST_DISK_ENCRYPTION_KEY, UnifiedEventType.DISK_ENCRYPTION_KEY_READ);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.ENABLED_MACOS_DISK_ENCRYPTION, UnifiedEventType.DISK_ENCRYPTION_ENABLED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.DISABLED_MACOS_DISK_ENCRYPTION, UnifiedEventType.DISK_ENCRYPTION_DISABLED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.ESCROWED_DISK_ENCRYPTION_KEY, UnifiedEventType.DISK_ENCRYPTION_KEY_ESCROWED);
+        
+        // macOS Profiles
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.CREATED_MACOS_PROFILE, UnifiedEventType.PROFILE_CREATED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.EDITED_MACOS_PROFILE, UnifiedEventType.PROFILE_UPDATED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.DELETED_MACOS_PROFILE, UnifiedEventType.PROFILE_DELETED);
+        
+        // macOS Setup Assistant
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.CHANGED_MACOS_SETUP_ASSISTANT, UnifiedEventType.CONFIGURATION_UPDATED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.DELETED_MACOS_SETUP_ASSISTANT, UnifiedEventType.CONFIGURATION_DELETED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.ENABLED_MACOS_SETUP_END_USER_AUTH, UnifiedEventType.CONFIGURATION_UPDATED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.DISABLED_MACOS_SETUP_END_USER_AUTH, UnifiedEventType.CONFIGURATION_UPDATED);
+        
+        // GitOps
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.ENABLED_GITOPS_MODE, UnifiedEventType.CONFIGURATION_UPDATED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.DISABLED_GITOPS_MODE, UnifiedEventType.CONFIGURATION_UPDATED);
+        
+        // Bootstrap Package
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.ADDED_BOOTSTRAP_PACKAGE, UnifiedEventType.CONFIGURATION_CREATED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.DELETED_BOOTSTRAP_PACKAGE, UnifiedEventType.CONFIGURATION_DELETED);
+        
+        // MDM Platform Support
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.ENABLED_WINDOWS_MDM, UnifiedEventType.MDM_ENABLED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.DISABLED_WINDOWS_MDM, UnifiedEventType.MDM_DISABLED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.ENABLED_ANDROID_MDM, UnifiedEventType.MDM_ENABLED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.DISABLED_ANDROID_MDM, UnifiedEventType.MDM_DISABLED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.ENABLED_WINDOWS_MDM_MIGRATION, UnifiedEventType.MDM_ENABLED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.DISABLED_WINDOWS_MDM_MIGRATION, UnifiedEventType.MDM_DISABLED);
+        
+        // Script Management
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.RAN_SCRIPT, UnifiedEventType.SCRIPT_EXECUTED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.ADDED_SCRIPT, UnifiedEventType.SCRIPT_CREATED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.EDITED_SCRIPT, UnifiedEventType.SCRIPT_UPDATED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.UPDATED_SCRIPT, UnifiedEventType.SCRIPT_UPDATED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.DELETED_SCRIPT, UnifiedEventType.SCRIPT_UPDATED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.CANCELED_RUN_SCRIPT, UnifiedEventType.SCRIPT_FAILED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.RAN_SCRIPT_BATCH, UnifiedEventType.BATCH_OPERATION_COMPLETED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.SCHEDULED_SCRIPT_BATCH, UnifiedEventType.BATCH_OPERATION_STARTED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.CANCELED_SCRIPT_BATCH, UnifiedEventType.BATCH_OPERATION_CANCELED);
+        
+        // Windows Profiles
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.CREATED_WINDOWS_PROFILE, UnifiedEventType.PROFILE_CREATED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.EDITED_WINDOWS_PROFILE, UnifiedEventType.PROFILE_UPDATED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.DELETED_WINDOWS_PROFILE, UnifiedEventType.PROFILE_DELETED);
+        
+        // Host Actions
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.LOCKED_HOST, UnifiedEventType.HOST_LOCKED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.UNLOCKED_HOST, UnifiedEventType.HOST_UNLOCKED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.WIPED_HOST, UnifiedEventType.HOST_WIPED);
+        
+        // Declaration Profiles
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.CREATED_DECLARATION_PROFILE, UnifiedEventType.PROFILE_CREATED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.EDITED_DECLARATION_PROFILE, UnifiedEventType.PROFILE_UPDATED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.DELETED_DECLARATION_PROFILE, UnifiedEventType.PROFILE_DELETED);
+        
+        // Configuration Profiles
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.RESENT_CONFIGURATION_PROFILE, UnifiedEventType.PROFILE_APPLIED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.RESENT_CONFIGURATION_PROFILE_BATCH, UnifiedEventType.BATCH_OPERATION_COMPLETED);
+        
+        // Software Management
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.INSTALLED_SOFTWARE, UnifiedEventType.SOFTWARE_INSTALLED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.UNINSTALLED_SOFTWARE, UnifiedEventType.SOFTWARE_UNINSTALLED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.ADDED_SOFTWARE, UnifiedEventType.SOFTWARE_CREATED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.EDITED_SOFTWARE, UnifiedEventType.SOFTWARE_UPDATED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.DELETED_SOFTWARE, UnifiedEventType.SOFTWARE_DELETED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.CANCELED_INSTALL_SOFTWARE, UnifiedEventType.SOFTWARE_INSTALLATION_CANCELED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.CANCELED_UNINSTALL_SOFTWARE, UnifiedEventType.SOFTWARE_INSTALLATION_CANCELED);
+        
+        // VPP (Volume Purchase Program)
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.ENABLED_VPP, UnifiedEventType.CONFIGURATION_UPDATED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.DISABLED_VPP, UnifiedEventType.CONFIGURATION_UPDATED);
+        
+        // App Store Apps
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.ADDED_APP_STORE_APP, UnifiedEventType.SOFTWARE_CREATED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.EDITED_APP_STORE_APP, UnifiedEventType.SOFTWARE_UPDATED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.DELETED_APP_STORE_APP, UnifiedEventType.SOFTWARE_DELETED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.INSTALLED_APP_STORE_APP, UnifiedEventType.SOFTWARE_INSTALLED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.CANCELED_INSTALL_APP_STORE_APP, UnifiedEventType.SOFTWARE_INSTALLATION_CANCELED);
+        
+        // SCEP Proxy
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.ADDED_NDES_SCEP_PROXY, UnifiedEventType.INTEGRATION_ADDED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.EDITED_NDES_SCEP_PROXY, UnifiedEventType.INTEGRATION_UPDATED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.DELETED_NDES_SCEP_PROXY, UnifiedEventType.INTEGRATION_DELETED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.ADDED_CUSTOM_SCEP_PROXY, UnifiedEventType.INTEGRATION_ADDED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.EDITED_CUSTOM_SCEP_PROXY, UnifiedEventType.INTEGRATION_UPDATED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.DELETED_CUSTOM_SCEP_PROXY, UnifiedEventType.INTEGRATION_DELETED);
+        
+        // Certificate Integrations
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.ADDED_DIGICERT, UnifiedEventType.INTEGRATION_ADDED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.EDITED_DIGICERT, UnifiedEventType.INTEGRATION_UPDATED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.DELETED_DIGICERT, UnifiedEventType.INTEGRATION_DELETED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.ADDED_HYDRANT, UnifiedEventType.INTEGRATION_ADDED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.EDITED_HYDRANT, UnifiedEventType.INTEGRATION_UPDATED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.DELETED_HYDRANT, UnifiedEventType.INTEGRATION_DELETED);
+        
+        // Conditional Access
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.ADDED_CONDITIONAL_ACCESS_INTEGRATION_MICROSOFT, UnifiedEventType.INTEGRATION_ADDED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.DELETED_CONDITIONAL_ACCESS_INTEGRATION_MICROSOFT, UnifiedEventType.INTEGRATION_DELETED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.ENABLED_CONDITIONAL_ACCESS_AUTOMATIONS, UnifiedEventType.AUTOMATION_ENABLED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.DISABLED_CONDITIONAL_ACCESS_AUTOMATIONS, UnifiedEventType.AUTOMATION_DISABLED);
+        
+        // Custom Variables
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.CREATED_CUSTOM_VARIABLE, UnifiedEventType.CONFIGURATION_CREATED);
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.DELETED_CUSTOM_VARIABLE, UnifiedEventType.CONFIGURATION_DELETED);
+        
+        // Setup Experience
+        registerMapping(IntegratedToolType.FLEET, SourceEventTypes.Fleet.EDITED_SETUP_EXPERIENCE_SOFTWARE, UnifiedEventType.CONFIGURATION_UPDATED);
     }
 }
--- openframe/services/openframe-stream/src/main/java/com/openframe/stream/mapping/SourceEventTypes.java
+++ openframe/services/openframe-stream/src/main/java/com/openframe/stream/mapping/SourceEventTypes.java
@@ -131,29 +131,173 @@ public interface SourceEventTypes {
         // Authentication events
         String USER_LOGGED_IN = "user_logged_in";
         String USER_FAILED_LOGIN = "user_failed_login";
+        String USER_ADDED_BY_SSO = "user_added_by_sso";
         
         // User management events
         String CREATED_USER = "created_user";
         String DELETED_USER = "deleted_user";
-        String EDITED_USER = "edited_user";
         String CHANGED_USER_GLOBAL_ROLE = "changed_user_global_role";
+        String DELETED_USER_GLOBAL_ROLE = "deleted_user_global_role";
+        String CHANGED_USER_TEAM_ROLE = "changed_user_team_role";
+        String DELETED_USER_TEAM_ROLE = "deleted_user_team_role";
         
-        // Device management events
+        // Device enrollment events
         String FLEET_ENROLLED = "fleet_enrolled";
-        String DELETED_HOST = "deleted_host";
-        String CHANGED_HOST_STATUS = "changed_host_status";
+        String MDM_ENROLLED = "mdm_enrolled";
+        String MDM_UNENROLLED = "mdm_unenrolled";
         
-        // Policy events
-        String POLICY_VIOLATION = "policy_violation";
-        String APPLIED_POLICY = "applied_policy";
-        String POLICY_COMPLIANCE_CHECKED = "policy_compliance_checked";
+        // Activity Automations
+        String ENABLED_ACTIVITY_AUTOMATIONS = "enabled_activity_automations";
+        String EDITED_ACTIVITY_AUTOMATIONS = "edited_activity_automations";
+        String DISABLED_ACTIVITY_AUTOMATIONS = "disabled_activity_automations";
         
-        // Remote session events
-        String REMOTE_SESSION_START = "remote_session_start";
-        String REMOTE_SESSION_END = "remote_session_end";
+        // Pack Management
+        String CREATED_PACK = "created_pack";
+        String EDITED_PACK = "edited_pack";
+        String DELETED_PACK = "deleted_pack";
+        String APPLIED_SPEC_PACK = "applied_spec_pack";
         
-        // Alerting events
-        String ALERT_TRIGGERED = "alert_triggered";
-        String ALERT_RESOLVED = "alert_resolved";
+        // Policy Management
+        String CREATED_POLICY = "created_policy";
+        String EDITED_POLICY = "edited_policy";
+        String DELETED_POLICY = "deleted_policy";
+        String APPLIED_SPEC_POLICY = "applied_spec_policy";
+        
+        // Saved Query Management
+        String CREATED_SAVED_QUERY = "created_saved_query";
+        String EDITED_SAVED_QUERY = "edited_saved_query";
+        String DELETED_SAVED_QUERY = "deleted_saved_query";
+        String DELETED_MULTIPLE_SAVED_QUERY = "deleted_multiple_saved_query";
+        String APPLIED_SPEC_SAVED_QUERY = "applied_spec_saved_query";
+        
+        // Team Management
+        String CREATED_TEAM = "created_team";
+        String DELETED_TEAM = "deleted_team";
+        String APPLIED_SPEC_TEAM = "applied_spec_team";
+        String TRANSFERRED_HOSTS = "transferred_hosts";
+        
+        // Agent Options
+        String EDITED_AGENT_OPTIONS = "edited_agent_options";
+        
+        // Queries
+        String LIVE_QUERY = "live_query";
+        
+        // OS Version Requirements
+        String EDITED_MACOS_MIN_VERSION = "edited_macos_min_version";
+        String EDITED_IOS_MIN_VERSION = "edited_ios_min_version";
+        String EDITED_IPADOS_MIN_VERSION = "edited_ipados_min_version";
+        String EDITED_WINDOWS_UPDATES = "edited_windows_updates";
+        
+        // Disk Encryption
+        String READ_HOST_DISK_ENCRYPTION_KEY = "read_host_disk_encryption_key";
+        String ENABLED_MACOS_DISK_ENCRYPTION = "enabled_macos_disk_encryption";
+        String DISABLED_MACOS_DISK_ENCRYPTION = "disabled_macos_disk_encryption";
+        String ESCROWED_DISK_ENCRYPTION_KEY = "escrowed_disk_encryption_key";
+        
+        // macOS Profiles
+        String CREATED_MACOS_PROFILE = "created_macos_profile";
+        String EDITED_MACOS_PROFILE = "edited_macos_profile";
+        String DELETED_MACOS_PROFILE = "deleted_macos_profile";
+        
+        // macOS Setup Assistant
+        String CHANGED_MACOS_SETUP_ASSISTANT = "changed_macos_setup_assistant";
+        String DELETED_MACOS_SETUP_ASSISTANT = "deleted_macos_setup_assistant";
+        String ENABLED_MACOS_SETUP_END_USER_AUTH = "enabled_macos_setup_end_user_auth";
+        String DISABLED_MACOS_SETUP_END_USER_AUTH = "disabled_macos_setup_end_user_auth";
+        
+        // GitOps
+        String ENABLED_GITOPS_MODE = "enabled_gitops_mode";
+        String DISABLED_GITOPS_MODE = "disabled_gitops_mode";
+        
+        // Bootstrap Package
+        String ADDED_BOOTSTRAP_PACKAGE = "added_bootstrap_package";
+        String DELETED_BOOTSTRAP_PACKAGE = "deleted_bootstrap_package";
+        
+        // MDM Platform Support
+        String ENABLED_WINDOWS_MDM = "enabled_windows_mdm";
+        String DISABLED_WINDOWS_MDM = "disabled_windows_mdm";
+        String ENABLED_ANDROID_MDM = "enabled_android_mdm";
+        String DISABLED_ANDROID_MDM = "disabled_android_mdm";
+        String ENABLED_WINDOWS_MDM_MIGRATION = "enabled_windows_mdm_migration";
+        String DISABLED_WINDOWS_MDM_MIGRATION = "disabled_windows_mdm_migration";
+        
+        // Script Management
+        String RAN_SCRIPT = "ran_script";
+        String ADDED_SCRIPT = "added_script";
+        String EDITED_SCRIPT = "edited_script";
+        String UPDATED_SCRIPT = "updated_script";
+        String DELETED_SCRIPT = "deleted_script";
+        String CANCELED_RUN_SCRIPT = "canceled_run_script";
+        String RAN_SCRIPT_BATCH = "ran_script_batch";
+        String SCHEDULED_SCRIPT_BATCH = "scheduled_script_batch";
+        String CANCELED_SCRIPT_BATCH = "canceled_script_batch";
+        
+        // Windows Profiles
+        String CREATED_WINDOWS_PROFILE = "created_windows_profile";
+        String EDITED_WINDOWS_PROFILE = "edited_windows_profile";
+        String DELETED_WINDOWS_PROFILE = "deleted_windows_profile";
+        
+        // Host Actions
+        String LOCKED_HOST = "locked_host";
+        String UNLOCKED_HOST = "unlocked_host";
+        String WIPED_HOST = "wiped_host";
+        
+        // Declaration Profiles
+        String CREATED_DECLARATION_PROFILE = "created_declaration_profile";
+        String EDITED_DECLARATION_PROFILE = "edited_declaration_profile";
+        String DELETED_DECLARATION_PROFILE = "deleted_declaration_profile";
+        
+        // Configuration Profiles
+        String RESENT_CONFIGURATION_PROFILE = "resent_configuration_profile";
+        String RESENT_CONFIGURATION_PROFILE_BATCH = "resent_configuration_profile_batch";
+        
+        // Software Management
+        String INSTALLED_SOFTWARE = "installed_software";
+        String UNINSTALLED_SOFTWARE = "uninstalled_software";
+        String ADDED_SOFTWARE = "added_software";
+        String EDITED_SOFTWARE = "edited_software";
+        String DELETED_SOFTWARE = "deleted_software";
+        String CANCELED_INSTALL_SOFTWARE = "canceled_install_software";
+        String CANCELED_UNINSTALL_SOFTWARE = "canceled_uninstall_software";
+        
+        // VPP (Volume Purchase Program)
+        String ENABLED_VPP = "enabled_vpp";
+        String DISABLED_VPP = "disabled_vpp";
+        
+        // App Store Apps
+        String ADDED_APP_STORE_APP = "added_app_store_app";
+        String EDITED_APP_STORE_APP = "edited_app_store_app";
+        String DELETED_APP_STORE_APP = "deleted_app_store_app";
+        String INSTALLED_APP_STORE_APP = "installed_app_store_app";
+        String CANCELED_INSTALL_APP_STORE_APP = "canceled_install_app_store_app";
+        
+        // SCEP Proxy
+        String ADDED_NDES_SCEP_PROXY = "added_ndes_scep_proxy";
+        String EDITED_NDES_SCEP_PROXY = "edited_ndes_scep_proxy";
+        String DELETED_NDES_SCEP_PROXY = "deleted_ndes_scep_proxy";
+        String ADDED_CUSTOM_SCEP_PROXY = "added_custom_scep_proxy";
+        String EDITED_CUSTOM_SCEP_PROXY = "edited_custom_scep_proxy";
+        String DELETED_CUSTOM_SCEP_PROXY = "deleted_custom_scep_proxy";
+        
+        // Certificate Integrations
+        String ADDED_DIGICERT = "added_digicert";
+        String EDITED_DIGICERT = "edited_digicert";
+        String DELETED_DIGICERT = "deleted_digicert";
+        String ADDED_HYDRANT = "added_hydrant";
+        String EDITED_HYDRANT = "edited_hydrant";
+        String DELETED_HYDRANT = "deleted_hydrant";
+        
+        // Conditional Access
+        String ADDED_CONDITIONAL_ACCESS_INTEGRATION_MICROSOFT = "added_conditional_access_integration_microsoft";
+        String DELETED_CONDITIONAL_ACCESS_INTEGRATION_MICROSOFT = "deleted_conditional_access_integration_microsoft";
+        String ENABLED_CONDITIONAL_ACCESS_AUTOMATIONS = "enabled_conditional_access_automations";
+        String DISABLED_CONDITIONAL_ACCESS_AUTOMATIONS = "disabled_conditional_access_automations";
+        
+        // Custom Variables
+        String CREATED_CUSTOM_VARIABLE = "created_custom_variable";
+        String DELETED_CUSTOM_VARIABLE = "deleted_custom_variable";
+        
+        // Setup Experience
+        String EDITED_SETUP_EXPERIENCE_SOFTWARE = "edited_setup_experience_software";
     }
 }

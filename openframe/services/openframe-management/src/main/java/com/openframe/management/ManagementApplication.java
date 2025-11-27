e9b29238062af4a980939e48e55d6c4ab8c09ba5
kirill-567 <kirill@flamingo.cx>
2025-11-27 12:22:39 +0100

    Management service core

--- openframe/services/openframe-management/src/main/java/com/openframe/management/ManagementApplication.java
+++ openframe/services/openframe-management/src/main/java/com/openframe/management/ManagementApplication.java
@@ -2,8 +2,24 @@ package com.openframe.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import com.openframe.data.health.CassandraHealthIndicator;
 
 @SpringBootApplication
+@ComponentScan(
+    basePackages = {
+        "com.openframe.management",
+        "com.openframe.data",
+        "com.openframe.core"
+    },
+    excludeFilters = {
+        @ComponentScan.Filter(
+            type = FilterType.ASSIGNABLE_TYPE,
+            classes = CassandraHealthIndicator.class
+        )
+    }
+)
 public class ManagementApplication {
     public static void main(String[] args) {
         SpringApplication.run(ManagementApplication.class, args);
